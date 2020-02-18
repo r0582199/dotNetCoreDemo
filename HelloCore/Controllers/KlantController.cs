b174bb77f1c649996a09f5351b13741a95df7b9d
r0582199 <r0582199@student.thomasmore.be>
2020-02-18 12:52:42 +0100

    Add project files.

---
+++ HelloCore/Controllers/KlantController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HelloCore.Models;
+
+namespace HelloCore.Controllers
+{
+    public class KlantController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Klant> vKLanten = new List<Klant>();
+
+            vKLanten.Add(new Klant() { Id = 1, Naam = "De Neve", Voornaam="Jos",AangemaaktDatum = new System.DateTime(2019,1,20) });
+            vKLanten.Add(new Klant() { Id = 2, Naam = "Bruynseels", Voornaam = "Jef", AangemaaktDatum = new System.DateTime(2019, 2, 4) });
+            vKLanten.Add(new Klant() { Id = 3, Naam = "Naert", Voornaam = "Jan", AangemaaktDatum = new System.DateTime(2019, 1, 5) });
+
+            return View(vKLanten);
+        }
+    }
+}
---
+++ HelloCore/Models/Klant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloCore.Models
+{
+    public class Klant
+    {
+        public int Id { get; set; }
+        public string Naam { get; set;}
+
+        public string Voornaam{ get; set; }        
+
+        [DataType(DataType.Date)]
+        public DateTime AangemaaktDatum { get; set; }
+    }
+}
